1c91c9e0340a31e90339c3b921932f3026fc9564
jesusSan1 <jjsanru3@gmail.com>
2024-04-26 11:30:18 -0600

    agregar formulario y creacion de layout

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de veterinario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 text-center text-upper mt-4 mb-3">
+                <h2>@yield('title')</h2>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="row">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,1 +1,28 @@
-Hola
+@extends('layouts.index')
+@section('title', 'Sistema de veterinario')
+@section('content')
+    <div class="col-md-4">
+        {{-- Form --}}
+        <form action="" method="post">
+            @csrf
+            <div class="form-group mb-3">
+                <label for="title">Titulo</label>
+                <input type="text" name="title" id="title" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+            <div class="form-group mb-3">
+                <label for="description">Descripcion</label>
+                <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+        </form>
+    </div>
+    <div class="col-md-8">
+        {{-- Table --}}
+    </div>
+@endsection
